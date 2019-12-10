3a181611694357cdf79472b825a0ce6aab0ec451
Jacob <jrwest@stanford.edu>
2019-12-09 22:49:13 -0800

    new script for pp set 2 for Wong

--- pp_Wong_set2.py
+++ pp_Wong_set2.py
@@ -12,7 +12,7 @@ import floatpy.derivatives.explicit.first as first_der
 import floatpy_helpers as fh
 
 #Local Modules
-import floatpy.readers.miranda_reader as mir
+import floatpy.readers.moving_grid_data_reader as mgdr
 
 # Set up default plotting
 params = {'legend.fontsize': 14,
@@ -23,95 +23,53 @@ matplotlib.rcParams.update(params)
 
 ########################################################################
 ###################### Functions ##############################
-def plot_set2(tsteps, tvec, path):
-    reader = mir.MirandaReader(path, periodic_dimensions=(False,True,True),verbose=True)
-
+def plot_set1(tsteps, tvec, path):
+    reader = mgdr.MovingGridDataReader(path)
     print("Domain Size: {} ".format(reader._domain_size))
-    print("Variables available: {}".format(reader.varNames))
+    print("Variables available: {}".format(reader._var_names))
     Nx, Ny, Nz = reader._domain_size
 
+    middle_selection = 0.12 #select middle 25% of domain in x
+    middle_offset = 0.016
+    x1 = int(Nx/2 - middle_selection/2*Nx + middle_offset*Nx)
+    x2 = int(Nx/2 + middle_selection/2*Nx + middle_offset*Nx)
+    reader.setSubDomain(((x1,0,0),(x2,Ny-1,Nz-1)))
+
+    # Get coordinates based on subdomain
+    x,y,z = reader.readCoordinates()
+    x_center = np.mean(x[:,0,0])
+    nx, ny, nz = reader._subdomain_hi-reader._subdomain_lo+1
+
     W        = np.zeros(len(tsteps))
-    
     t_legend1 = []
     t_legend2 = []
-
+    
     for tind, step in enumerate(tsteps):
 
-        ##################################
-        #Reading in full fields to figure out where subdomain and interface are
-        reader.setStep(step)
-        reader.setSubDomain(((0,0,0),(Nx-1,Ny-1,Nz-1)))
-        print("Figuring out subdomain at step {}".format(step))
-        YN2  = np.squeeze(np.array(reader.readData('Ideal1_01')))
-        YO2  = np.squeeze(np.array(reader.readData('Ideal1_02')))
-        YSF6 = np.squeeze(np.array(reader.readData('Ideal1_03')))
-        YAc  = np.squeeze(np.array(reader.readData('Ideal1_04')))
-        YHeavy = YSF6+YAc
-
-        M = (YN2/M_N2+YO2/M_O2+YSF6/M_SF6+YAc/M_Ac)**(-1)
-
-        #Find inner mixing zone (IMZ)
-        IMZ_thresh = 0.9
-        XHeavy = M/M_Heavy*YHeavy
-        XHeavy_bar = np.mean(np.mean(XHeavy,axis=2),axis=1)
-        IMZ_crit = 4*XHeavy_bar*(1-XHeavy_bar) - IMZ_thresh
-
-        for ind in range(len(IMZ_crit)):
-            if IMZ_crit[ind] >= 0:
-                IMZ_lo = ind
-                break
-
-        for ind in range(len(IMZ_crit)):
-            ind2 = Nx-ind-1
-            if IMZ_crit[ind2] >= 0:
-                IMZ_hi = ind2
-                break
-
-        IMZ_mid = np.argmax(IMZ_crit)
-
-        middle_selection = 0.40 #select middle 20% of domain in x
-        x1 = IMZ_mid-int(middle_selection/2*Nx)
-        x2 = min(IMZ_mid+int(middle_selection/2*Nx),Nx-1)
-        reader.setSubDomain(((x1,0,0),(x2,Ny-1,Nz-1)))
-
-        # Get coordinates based on subdomain
-        x,y,z = reader.readCoordinates()
-        x = x/100.0 #convert from cm-> m
-        y = y/100.0 
-        z = z/100.0
-        nx = reader.chunk[0][1] - reader.chunk[0][0]
-        ny = reader.chunk[1][1] - reader.chunk[1][0]
-        nz = reader.chunk[2][1] - reader.chunk[2][0]
-
-        ##################################
-
-        # Reading in on subdomain  
+        #Reading in
         reader.setStep(step)
         print("reading in Mass Fraction at step {}.".format(step))
-        YN2  = np.squeeze(np.array(reader.readData('Ideal1_01')))
-        YO2  = np.squeeze(np.array(reader.readData('Ideal1_02')))
-        YSF6 = np.squeeze(np.array(reader.readData('Ideal1_03')))
-        YAc  = np.squeeze(np.array(reader.readData('Ideal1_04')))
-        YHeavy = YSF6+YAc
-        
+        YHeavy       = np.squeeze(np.array(reader.readData('mass fraction 0')))
         print("reading in density at step {}.".format(step))
-        density    = 1e3*np.squeeze(np.array(reader.readData('density')))
+        density    = np.squeeze(np.array(reader.readData('density')))
         print("reading in pressure at step {}.".format(step))
-        pressure   = 1e-1*np.squeeze(np.array(reader.readData('pressure')))
+        pressure   = np.squeeze(np.array(reader.readData('pressure')))
         
         #Calculating Stats
 	print("Computing Diffusion Properties")
         rho_bar = np.mean(np.mean(density,axis=2),axis=1)
 
-        Cp = YN2*Cp_N2 +YO2*Cp_O2 +YSF6*Cp_SF6 +YAc*Cp_Ac
-        Cv = YN2*Cv_N2 +YO2*Cv_O2 +YSF6*Cv_SF6 +YAc*Cv_Ac
-
+        Cp = YHeavy*Cp_Heavy + (1-YHeavy)*Cp_air
+        Cv = YHeavy*Cv_Heavy + (1-YHeavy)*Cv_air
         gamma = Cp/Cv
-        M = (YN2/M_N2+YO2/M_O2+YSF6/M_SF6+YAc/M_Ac)**(-1)
-        T = M*pressure/(density*Ru)
+	del Cp, Cv
+ 
         c = (gamma*pressure/density)**0.5
-       
-        del YAc, gamma
+
+        del gamma
+
+        M = (YHeavy/M_Heavy+(1-YHeavy)/M_air)**(-1)
+        T = M*pressure/(density*Ru)
 
         #Find inner mixing zone (IMZ)
         IMZ_thresh = 0.9
@@ -136,37 +94,33 @@ def plot_set2(tsteps, tvec, path):
 	print("Computing Mixing Width and Mixedness")
         integrand = 4*XHeavy_bar*(1-XHeavy_bar)
         W[tind] = integrate.simps(integrand,x[:,0,0])
-        
         xstar= (x[:,0,0]-x[IMZ_mid,0,0])/W[tind]
 
         #Mole Fraction Variance
         XHeavy_prime = XHeavy-XHeavy_bar.reshape((nx,1,1))
         XHeavy_variance = np.mean(np.mean(XHeavy_prime**2,axis=2),axis=1)
 
-        if step < 22:
+        if step < 125:
             #before reshock
             plt.figure(1)
-            t_legend1.append("t = {} ms".format(step*1e-1))
+            t_legend1.append("t = {} ms".format(step*1e-2))
         else:
             #after reshock
             plt.figure(2)
-            t_legend2.append("t = {} ms".format(step*1e-1))
+            t_legend2.append("t = {} ms".format(step*1e-2))
 
         plt.plot(xstar,XHeavy_variance**0.5)
         plt.xlim([-2,2])
         plt.xlabel('x*')
-        plt.ylabel("X'rms (SF6+Ac)")
-        plt.ylim([0,0.4])
+        plt.ylabel("X'rms (SF6)")
+        plt.ylim([0,0.3])
 
         del XHeavy
 
         #Compute Turbulent Mach Number
         print("reading in velocity at step {}.".format(step))
 	print("Computing Turbulent Mach Number")
-        velocity = np.zeros((3,nx,ny,nz))
-        velocity[0,:,:,:]   = 1e-2*np.squeeze(np.array(reader.readData('velocity-0')))
-        velocity[1,:,:,:]   = 1e-2*np.squeeze(np.array(reader.readData('velocity-1')))
-        velocity[2,:,:,:]   = 1e-2*np.squeeze(np.array(reader.readData('velocity-2')))
+        velocity   = np.squeeze(np.array(reader.readData('velocity')))
         u_tilde = np.mean(np.mean(velocity[0,:,:,:]*density,axis=2),axis=1)/np.mean(np.mean(density,axis=2),axis=1)
         v_tilde = np.mean(np.mean(velocity[1,:,:,:]*density,axis=2),axis=1)/np.mean(np.mean(density,axis=2),axis=1)
         w_tilde = np.mean(np.mean(velocity[2,:,:,:]*density,axis=2),axis=1)/np.mean(np.mean(density,axis=2),axis=1)
@@ -184,7 +138,12 @@ def plot_set2(tsteps, tvec, path):
         Rkk = np.mean(np.mean(2*TKE,axis=2),axis=1)/np.mean(np.mean(density,axis=2),axis=1)
         b11 = R11/Rkk - 1.0/3.0
 
-        if step < 22:
+        Ma_t = (((np.mean(np.mean(2*TKE/density,axis=2),axis=1))**0.5)/
+             np.mean(np.mean(c,axis=2),axis=1))
+
+        del u_doubleprime,v_doubleprime,w_doubleprime, TKE
+
+        if step < 125:
             #before reshock
             plt.figure(3)
         else:
@@ -196,17 +155,12 @@ def plot_set2(tsteps, tvec, path):
         plt.ylabel('b11')
         plt.xlim([-1.5,1.5])
         plt.ylim([-2.0/3.0,2.0/3.0])
-
-        Ma_t = (((np.mean(np.mean(2*TKE/density,axis=2),axis=1))**0.5)/
-             np.mean(np.mean(c,axis=2),axis=1))
-
-        del u_doubleprime,v_doubleprime,w_doubleprime, TKE
     
         #Compute Effective Atwood Number
         rho_prime = density-rho_bar.reshape((nx,1,1))
 
         At_e = ((np.mean(np.mean(rho_prime**2,axis=2),axis=1))**0.5)/rho_bar
-        
+
         # Compute Covariances of Density
         M_bar = np.mean(np.mean(M,axis=2),axis=1)
         M_prime = M-M_bar.reshape((nx,1,1))
@@ -222,39 +176,39 @@ def plot_set2(tsteps, tvec, path):
 
         del rho_prime, M, M_prime, T, Tinv_prime, p_prime
 
-        if (step == 15) or (step == 22):
+        if (step == 80) or (step == 135):
             plt.figure(step)
             plt.plot(xstar,cov_rho_p)
             plt.plot(xstar,cov_rho_M)
             plt.plot(xstar,cov_rho_Tinv)
             plt.xlabel('x*')
             plt.xlim([-2.0,2.0])
-            plt.ylim([0,.35])
+            plt.ylim([0,0.25])
             plt.legend(['rho-p','rho-M','rho-1/T'])
             plt.ylabel('Density Covariance')
-            plt.title("t = {} ms".format(step*1e-1))
-        
+            plt.title("t = {} ms".format(step*1e-2))
+
         #Density Reconstruction
-        rho_Heavy = np.mean(density[-1,:,:])
-        rho_air = np.mean(density[0,:,:])
+        rho_Heavy = np.mean(density[0,:,:])
+        rho_air = np.mean(density[-1,:,:])
         rho_recon_bar = (rho_Heavy-rho_air)*XHeavy_bar+rho_air
-
-        print(rho_air,rho_Heavy)
         
-        if (step == 15) or (step==50):
-            plt.figure(5)
-            plt.plot(xstar,rho_recon_bar/rho_bar)
-            plt.xlabel('x*')
-            plt.ylabel('rho / incompressible rho')
-            plt.legend(['t = 1.5 ms','t = 5 ms'])
-
+        if step < 125:
             #before reshock
+            plt.figure(5)
+        else:
+            #after reshock
+            plt.figure(6)
 
-        del velocity, pressure
+        plt.plot(xstar,rho_recon_bar/rho_bar)
+        plt.xlabel('x*')
+        plt.ylabel('rho / incompressible rho')
+        
+        del density, YHeavy
 
     #Format plots
     print("Formatting Plots")
-    for ind in range(1,5):
+    for ind in range(1,7):
         plt.figure(ind)
         if ind%2 == 1:
             plt.legend(t_legend1)
@@ -264,6 +218,7 @@ def plot_set2(tsteps, tvec, path):
     for ind in plt.get_fignums():
         plt.figure(ind)
         plt.tight_layout()
+
 
     return None
 
@@ -276,15 +231,17 @@ if __name__ == '__main__':
     ########################################################################
     ########################################################################
     #### PROBLEM SPECIFIC STUFF ####
-    from Tritschler_setup import *
+    from Wong_setup import *
     
     #Choose the time steps to use
-    tsteps = (15,20,22,27,50)
-    tvec   = 1e-4*np.array(tsteps)
-    path = "/home/jrwest/Research/FloATPy_moving_grid/data/Tritschler/RM_CTR_3D_64/plot.mir"
+    tsteps = (10,45,80,115,125,135,155,180) #all steps
+    
+    #path = "/work/05428/tg847275/Wong/uniform_data/3D_Poggi_RMI_RD/case_1_1/grid_B"
+    path = "/home/jrwest/Research/FloATPy_moving_grid/data/Wong/grid_B"
 
-    plot_set2(tsteps,tvec,path)
+    tvec = 1e-5*np.array(tsteps)
 
+    plot_set1(tsteps,tvec,path)
 
     #### END PROBLEM SPECIFIC STUFF ####
     ########################################################################
